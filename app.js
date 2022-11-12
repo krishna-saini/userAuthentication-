3ef9f02d1c0e495469a29b2690e9131dcdee772b
krishna-saini <krishnapraanjal10@gmail.com>
2022-11-13 00:22:12 +0530

    added signup controller logic

--- app.js
+++ app.js
@@ -18,7 +18,44 @@ app.get("/", (req, res) => {
   res.status(200).send("welcome on homepage");
 });
 
-
+// routing for registration
+app.post("/registration", async (req, res) => {
+    try {
+      // 1. get all data from req.body
+      const { name, email, password } = req.body;
+  
+      // 2. validate
+      if (!(email || password)) {
+        res.status(400).send("enter email and/or password");
+      }
+  
+      // 3. check if user exists in db
+      const existingUser = await User.findOne({ email: email });
+      console.log(existingUser);
+      if (existingUser) {
+        res.status(400).send("user already exists. please login");
+        return;
+      }
+  
+      // 4. encrypt the password
+      const encryptedPassword = await bcrypt.hash(password, 10);
+  
+      // 5. save to db and send a token
+      // saving to db
+      const user = await User.create({
+        name,
+        email,
+        password: encryptedPassword,
+      });
+  
+      // creating a token using jwt
+      let token = jwt.sign({ id: user._id }, `${process.env.SECRET}`);
+      // console.log(token);
+      res.send("success");
+    } catch (err) {
+      console.log(err);
+    }
+  });
 // create server
 app.listen(4000, () => {
   console.log("listening on port");
